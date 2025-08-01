e5dd2af3a4d2394303c1574dff73e772c11962cc
rajasC2006 <87762147+rajasC2006@users.noreply.github.com>
2025-08-01 14:02:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const app = express();
 app.use(express.json());
 
 // Set port and verify_token
+const WHATSAPP_ACCESS_TOKEN =
+  "EAASbflZCFgYkBPI41pNRbrdZAtvSfnUx4FDbch6gT3iwYE1vrDip9wvYbu1DAiUwj0SZA8GK84X0U694oJc9j5kKOM9BRLdqVfBD0BA4BxJJBTLXdhggY4lYWNrRMe5ClFLDIbtSOa6M9Lc8gONUsjPggGSt5APwElMAHQIxROic1VCsBK2CVVzhYPDuAZDZD";
 const port = process.env.PORT || 3000;
+const URL="https://graph.facebook.com/v22.0/645746375298493/messages"
 const verifyToken = process.env.VERIFY_TOKEN;
 
 // Route for GET requests
@@ -36,10 +39,11 @@ app.post('/webhook', (req, res) => {
   const name1 = changes[0].value.contacts
     ? changes[0].value.contacts[0].profile
     : null;
-if(statuses)
-  console.log(JSON.stringify(statuses,null,2));
+// if(statuses)
+//   console.log(JSON.stringify(statuses,null,2));
 if(message)
   console.log(JSON.stringify(message, null, 2));
+  sendcatalog(message.from,name1.name);
   res.status(200).end();
 });
 
@@ -47,3 +51,28 @@ if(message)
 app.listen(port, () => {
   console.log(`\nListening on port ${port}\n`);
 });
+async function sendCatalog(to, name1) {
+  await axios({
+    url: `${URL}`,
+    method: "post",
+    headers: {
+      Authorization: `Bearer ${WHATSAPP_ACCESS_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify({
+      messaging_product: "whatsapp",
+      to,
+      type: "interactive",
+      interactive: {
+        type: "catalog_message",
+        body: {
+          text: `Hello ${name1},Welcome to TechB's Shopping Experience.\n\nTo start shopping, you can view our catalog and add items to purchase.
+          \n\n*You can directly place your order on WhatsApp chat🥳🥳.* \n\nClick the View Catalog button to start shopping.`,
+        },
+        action: {
+          name: "catalog_message",
+        },
+      },
+    }),
+  });
+}
